004b4f6cb0265d383627d9b2f5bc6a8433cb63c3
Berat Aras <berat.aras@hotmail.com>
2021-11-13 20:43:17 +0300

    added: static public file

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function (){
-    console.log('sa')
-}
+const express = require('express')
+const path = require("path");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`server started at ${port} port`);
+})
+
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve('temp/index.html'))
+})
